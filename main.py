57c8b21e6d88bac85a3208fd96140271ff21b201
KanonKC <Kanonkce@gmail.com>
2022-06-26 14:07:12 +0700

    Attachment DB

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from FileOp import *
 from DataModel import *
-from routes import message_detector,nxo,homeworklist,youtube,mint_tutor
+from routes import message_detector,homeworklist,youtube,mint_tutor
 
 app = FastAPI()
 app.include_router(message_detector.router)
-app.include_router(nxo.router)
+# app.include_router(nxo.router)
 app.include_router(homeworklist.router)
 app.include_router(youtube.router)
 app.include_router(mint_tutor.router)
